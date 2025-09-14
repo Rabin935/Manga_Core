474cf16fe838f187366812b8687564c08c6bed5f
Rabin Tamang <lrabin935@gmail.com>
2025-09-14 19:43:49 +0545

    typescript with tailwind and node express installed

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Backend running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="text-4xl text-blue-600 font-bold transition duration-500 
+         hover:text-red-500 hover:scale-105 ">
+    Hello world!
+  </div>
+    </>
+  )
+}
+
+export default App
